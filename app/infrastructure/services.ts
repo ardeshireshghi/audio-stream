3e760efa48ff7882c497448496045b91cb5e12f7
Ardeshir Eshghi <ardi.eshghi@gmail.com>
2022-02-16 01:52:07 +0000

    Injects `blobStoreClient` to the services

--- app/infrastructure/services.ts
+++ app/infrastructure/services.ts
@@ -1,19 +1,27 @@
 import createBlobStoreClient, {
+  BlobStoreClient,
   BlobStoreTypes
 } from '../../lib/blob-store/src/BlobStoreClient';
 import { TrackBlobStoreService } from '../interfaces/services/TrackBlobStoreService';
 import { TrackFileSystemService } from '../interfaces/services/TrackFileSystemService';
+import { TrackService } from '../interfaces/services/TrackService';
 
-const createServices = () => {
-  const s3Client = createBlobStoreClient(
+type Services = {
+  blobStoreClient: BlobStoreClient;
+  trackService: TrackService;
+};
+
+const createServices = (): Services => {
+  const blobStoreClient = createBlobStoreClient(
     BlobStoreTypes.S3,
     process.env.MEDIA_PERSIST_S3_BUCKET_NAME,
     'audio-stream-files/'
   );
 
   return {
+    blobStoreClient,
     trackService: process.env.MEDIA_PERSIST_S3_BUCKET_NAME
-      ? new TrackBlobStoreService(s3Client)
+      ? new TrackBlobStoreService(blobStoreClient)
       : new TrackFileSystemService()
   };
 };
